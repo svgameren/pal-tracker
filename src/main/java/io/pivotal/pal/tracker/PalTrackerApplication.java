5f17ee2baf3d4b00deeb4dbea463f56fd0feb718
Stian van Gameren <bvangameren@mirabeau.nl>
2017-08-16 14:57:27 +0200

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,12 @@
 package io.pivotal.pal.tracker;
 
+import org.mariadb.jdbc.MariaDbDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -12,7 +16,9 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository getTimeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository getTimeEntryRepository() throws SQLException {
+        DataSource dataSource = new MariaDbDataSource(System.getenv("SPRING_DATASOURCE_URL"));
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
